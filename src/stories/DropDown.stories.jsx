5b0db8557dab5545043287f4fe125422547e61ea
S Varun <47653646+VarunSriram99@users.noreply.github.com>
2022-03-29 13:00:30 +0530

    Fixed dropdown issue (#16)

--- src/stories/DropDown.stories.jsx
+++ src/stories/DropDown.stories.jsx
@@ -42,41 +42,41 @@ export const AllVariants = () => (
         <div className='flex flex-col items-center border border-dotted border-blue-500 p-4 gap-4'>
             <Typography style="h4" type="boldUppercase" className="whitespace-nowrap" >Primary Button Styles</Typography>
             <Typography style="h5" type="semibold" className="whitespace-nowrap" >Regular Dropdown</Typography>
-            <DropDown label="DropDown"><li>Option 1</li><l1>Option 2</l1></DropDown>
+            <DropDown label="DropDown"><li>Option 1</li><li>Option 2</li></DropDown>
             <Typography style="h5" type="semibold" className="whitespace-nowrap" >Dropdown with icon on left</Typography>
-            <DropDown label="DropDown" icon={RightArrow}><li>Option 1</li><l1>Option 2</l1></DropDown>
+            <DropDown label="DropDown" icon={RightArrow}><li>Option 1</li><li>Option 2</li></DropDown>
             <Typography style="h5" type="semibold" className="whitespace-nowrap" >Dropdown with icon on right</Typography>
-            <DropDown label="DropDown" icon={RightArrow} buttonProps={{iconPosition: "right"}} ><li>Option 1</li><l1>Option 2</l1></DropDown>
+            <DropDown label="DropDown" icon={RightArrow} buttonProps={{iconPosition: "right"}} ><li>Option 1</li><li>Option 2</li></DropDown>
             <Typography style="h5" type="semibold" className="whitespace-nowrap" >Dropdown with Dropdown strategy absolute</Typography>
-            <DropDown label="DropDown" position="bottom" ><li>Option 1</li><l1>Option 2</l1></DropDown>
+            <DropDown label="DropDown" position="bottom" ><li>Option 1</li><li>Option 2</li></DropDown>
             <Typography style="h5" type="semibold" className="whitespace-nowrap" >Dropdown with Dropdown strategy fixed</Typography>
-            <DropDown label="DropDown" position="bottom" strategy='fixed' ><li>Option 1</li><l1>Option 2</l1></DropDown>
+            <DropDown label="DropDown" position="bottom" strategy='fixed' ><li>Option 1</li><li>Option 2</li></DropDown>
         </div>
         <div className='flex flex-col items-center border border-dotted border-blue-500 p-4 gap-4'>
             <Typography style="h4" type="boldUppercase" className="whitespace-nowrap" >Primary Button Styles</Typography>
             <Typography style="h5" type="semibold" className="whitespace-nowrap" >Regular Dropdown</Typography>
-            <DropDown label="DropDown" buttonStyle="secondary"><li>Option 1</li><l1>Option 2</l1></DropDown>
+            <DropDown label="DropDown" buttonStyle="secondary"><li>Option 1</li><li>Option 2</li></DropDown>
             <Typography style="h5" type="semibold" className="whitespace-nowrap" >Dropdown with icon on left</Typography>
-            <DropDown label="DropDown" icon={RightArrow} buttonStyle="secondary"><li>Option 1</li><l1>Option 2</l1></DropDown>
+            <DropDown label="DropDown" icon={RightArrow} buttonStyle="secondary"><li>Option 1</li><li>Option 2</li></DropDown>
             <Typography style="h5" type="semibold" className="whitespace-nowrap" >Dropdown with icon on right</Typography>
-            <DropDown label="DropDown" icon={RightArrow} buttonProps={{iconPosition: "right"}} buttonStyle="secondary" ><li>Option 1</li><l1>Option 2</l1></DropDown>
+            <DropDown label="DropDown" icon={RightArrow} buttonProps={{iconPosition: "right"}} buttonStyle="secondary" ><li>Option 1</li><li>Option 2</li></DropDown>
             <Typography style="h5" type="semibold" className="whitespace-nowrap" >Dropdown with Dropdown strategy absolute</Typography>
-            <DropDown label="DropDown" position="bottom" buttonStyle="secondary" ><li>Option 1</li><l1>Option 2</l1></DropDown>
+            <DropDown label="DropDown" position="bottom" buttonStyle="secondary" ><li>Option 1</li><li>Option 2</li></DropDown>
             <Typography style="h5" type="semibold" className="whitespace-nowrap" >Dropdown with Dropdown strategy fixed</Typography>
-            <DropDown label="DropDown" position="bottom" strategy='fixed' buttonStyle="secondary" ><li>Option 1</li><l1>Option 2</l1></DropDown>
+            <DropDown label="DropDown" position="bottom" strategy='fixed' buttonStyle="secondary" ><li>Option 1</li><li>Option 2</li></DropDown>
         </div>
         <div className='flex flex-col items-center border border-dotted border-blue-500 p-4 gap-4'>
             <Typography style="h4" type="boldUppercase" className="whitespace-nowrap" >Primary Button Styles</Typography>
             <Typography style="h5" type="semibold" className="whitespace-nowrap" >Regular Dropdown</Typography>
-            <DropDown label="DropDown" buttonStyle="text"><li>Option 1</li><l1>Option 2</l1></DropDown>
+            <DropDown label="DropDown" buttonStyle="text"><li>Option 1</li><li>Option 2</li></DropDown>
             <Typography style="h5" type="semibold" className="whitespace-nowrap" >Dropdown with icon on left</Typography>
-            <DropDown label="DropDown" icon={RightArrow} buttonStyle="text"><li>Option 1</li><l1>Option 2</l1></DropDown>
+            <DropDown label="DropDown" icon={RightArrow} buttonStyle="text"><li>Option 1</li><li>Option 2</li></DropDown>
             <Typography style="h5" type="semibold" className="whitespace-nowrap" >Dropdown with icon on right</Typography>
-            <DropDown label="DropDown" icon={RightArrow} buttonProps={{iconPosition: "right"}} buttonStyle="text" ><li>Option 1</li><l1>Option 2</l1></DropDown>
+            <DropDown label="DropDown" icon={RightArrow} buttonProps={{iconPosition: "right"}} buttonStyle="text" ><li>Option 1</li><li>Option 2</li></DropDown>
             <Typography style="h5" type="semibold" className="whitespace-nowrap" >Dropdown with Dropdown strategy absolute</Typography>
-            <DropDown label="DropDown" position="bottom" buttonStyle="text" ><li>Option 1</li><l1>Option 2</l1></DropDown>
+            <DropDown label="DropDown" position="bottom" buttonStyle="text" ><li>Option 1</li><li>Option 2</li></DropDown>
             <Typography style="h5" type="semibold" className="whitespace-nowrap" >Dropdown with Dropdown strategy fixed</Typography>
-            <DropDown label="DropDown" position="bottom" strategy='fixed' buttonStyle="text" ><li>Option 1</li><l1>Option 2</l1></DropDown>
+            <DropDown label="DropDown" position="bottom" strategy='fixed' buttonStyle="text" ><li>Option 1</li><li>Option 2</li></DropDown>
         </div>
     </div>
 )
